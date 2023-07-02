4e9b0a72f8bba9e7ad872c75d653f5b063a3a3b5
Mohamad Mourad <M.Mouradis511@gmail.com>
2023-07-02 20:59:01 +0300

    init

---
+++ variables.js
@@ -0,0 +1,26 @@
+let all = document.getElementsByClassName('divs');
+let result = document.getElementById('result');
+let resultBox = document.getElementById('resultBox');
+let again = document.getElementById('again');
+
+again.addEventListener('click', function(){
+    resultBox.style.display = "none";
+    for(let i = 0;i<all.length;i++){
+        all[i].style.backgroundColor = "blanchedalmond";
+        all[i].innerHTML = "";
+    }
+     turn = "X";
+    turnCounter = 0;
+    moveCounter = 0;
+    isVictory = true;
+   endGameCounter = 0; 
+    comparisiontArray = [[10,11,12],     
+                         [13,14,15],
+                         [16,17,18]];
+
+});
+
+
+let comparisiontArray = [[10,11,12],     
+                         [13,14,15],
+                         [16,17,18]];
